e4bc77c2eca29bc780d35a83bda77923ce8be594
Sv02v <Se02vas@gmail.com>
2024-10-13 22:39:01 -0600

    Update Persona.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Persona.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Persona.java
@@ -10,28 +10,31 @@ package pcscpracticaprogramada.pkg1;
  */
 public class Persona {
     protected String nombre;
+    // Atributo protegido que almacena el email de la persona
     protected String email;
 
+    // Constructor que inicializa el nombre y el email de la persona
     public Persona(String nombre, String email) {
-        this.nombre = nombre;
-        this.email = email;
+        this.nombre = nombre; // Asigna el nombre proporcionado
+        this.email = email; // Asigna el email proporcionado
     }
 
+    // Método para obtener el nombre de la persona
     public String getNombre() {
         return nombre;
     }
 
+    // Método para cambiar el nombre de la persona
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Método para obtener el email de la persona
     public String getEmail() {
         return email;
     }
 
+    // Método para cambiar el email de la persona
     public void setEmail(String email) {
         this.email = email;
     }
-    
-    
-}
