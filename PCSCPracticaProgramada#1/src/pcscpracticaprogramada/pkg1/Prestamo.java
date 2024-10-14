b7aa73c5df214731892eed32eadede2c10ba7602
Sv02v <Se02vas@gmail.com>
2024-10-13 22:40:33 -0600

    Update Prestamo.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Prestamo.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Prestamo.java
@@ -9,14 +9,16 @@ package pcscpracticaprogramada.pkg1;
  * @author Andy
  */
 public abstract class Prestamo {
-    protected Libro libro;
+     protected Libro libro;
+    // Atributo protegido que almacena el usuario que realiza el préstamo
     protected Usuario usuario;
 
+    // Constructor que inicializa el libro y el usuario del préstamo
     public Prestamo(Libro libro, Usuario usuario) {
-        this.libro = libro;
-        this.usuario = usuario;
+        this.libro = libro; // Asigna el libro proporcionado
+        this.usuario = usuario; // Asigna el usuario proporcionado
     }
     
-    
+    // Método abstracto que calcula el costo del préstamo
+    // Este método debe ser implementado por las clases que hereden de Prestamo
     public abstract int calcularCosto();
-}
