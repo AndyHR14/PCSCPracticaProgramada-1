0930a48df69fd8b2ea03068b594ba21db449374a
Sv02v <Se02vas@gmail.com>
2024-10-13 22:43:13 -0600

    Update Usuario.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Usuario.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Usuario.java
@@ -9,32 +9,33 @@ package pcscpracticaprogramada.pkg1;
  * @author Andy
  */
 public class Usuario extends Persona{
+      // Atributo protegido que almacena la fecha de registro del usuario
     protected String fechaDeRegistro;
 
-
+    // Constructor de la clase Usuario, que recibe la fecha de registro, nombre y email
     public Usuario(String fechaDeRegistro, String nombre, String email) {
-        super(nombre, email);
-        this.fechaDeRegistro = fechaDeRegistro;
+        super(nombre, email); // Llama al constructor de la clase Persona para inicializar nombre y email
+        this.fechaDeRegistro = fechaDeRegistro; // Asigna la fecha de registro
     }
     
-    public String getEmail(){
+    // Método para obtener el email del usuario
+    @Override
+    public String getEmail() {
         return email;
     }
 
+    // Método para obtener la fecha de registro del usuario
     public String getFechaDeRegistro() {
         return fechaDeRegistro;
     }
 
+    // Método para establecer una nueva fecha de registro del usuario
     public void setFechaDeRegistro(String fechaDeRegistro) {
         this.fechaDeRegistro = fechaDeRegistro;
     }
 
+    // Método para obtener el nombre del usuario
+    @Override
     public String getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
-}
