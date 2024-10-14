ff442742158ae7b1d9f0bcc419c68ebc73390ea1
Sv02v <Se02vas@gmail.com>
2024-10-13 22:36:34 -0600

    Update Autor.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Autor.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Autor.java
@@ -11,44 +11,45 @@ import java.util.List;
  *
  * @author Andy
  */
-public class Autor {
-    private String nombre;
-    private String apellido;
-    private List<Libro> libros;
-
+// Constructor que inicializa el nombre, el apellido y crea la lista de libros
     public Autor(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.libros = new ArrayList<>();
+        this.libros = new ArrayList<>(); // Se inicia con una lista vacía de libros
     }
     
-   public void agregarLibro(Libro libro){
-       libros.add(libro);
-   }
+    // Método para agregar un libro a la lista de libros del autor
+    public void agregarLibro(Libro libro){
+       libros.add(libro); // Agrega el libro proporcionado a la lista
+    }
 
+    // Método para obtener el nombre del autor
     public String getNombre() {
         return nombre;
     }
 
+    // Método para cambiar el nombre del autor
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Método para obtener el apellido del autor
     public String getApellido() {
         return apellido;
     }
 
+    // Método para cambiar el apellido del autor
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    // Método para obtener la lista de libros del autor
     public List<Libro> getLibros() {
         return libros;
     }
 
+    // Método para establecer una lista de libros y reemplaza la actual
     public void setLibros(List<Libro> libros) {
         this.libros = libros;
     }
-
-    
 }
