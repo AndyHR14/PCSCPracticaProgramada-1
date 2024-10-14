51b494b1e70092f76eda591e7e5f383fb7313479
Sv02v <Se02vas@gmail.com>
2024-10-13 22:37:32 -0600

    Update Gestion.java

--- PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Gestion.java
+++ PCSCPracticaProgramada#1/src/pcscpracticaprogramada/pkg1/Gestion.java
@@ -1,19 +1,22 @@
 
 package pcscpracticaprogramada.pkg1;
 
-
-public class Gestion extends Libro{
+public class Gestion extends Libro {
+    // Arreglo que almacena varios libros
     private Libro[] libros;
- 
+
+    // Constructor de la clase Gestion que inicializa el arreglo de libros y los atributos heredados de Libro
     public Gestion(Libro[] libros, String titulo, Autor autor, int anioPublicacion, boolean disponible) {
+        // Llama al constructor de la clase padre (Libro) para establecer título, autor, año y disponibilidad
         super(titulo, autor, anioPublicacion, disponible);
-        this.libros = libros;
+        this.libros = libros; // Guarda el arreglo de libros proporcionado
     }
     
-    
-public void mostrarLibros(){
- for (int i = 0; i > libros.length; i++){
-     
-       }   
+    // Método para mostrar los libros almacenados en el arreglo
+    public void mostrarLibros() {
+        // Recorre el arreglo de libros usando un bucle for
+        for (int i = 0; i > libros.length; i++) {
+            // Aquí debería ir el código para mostrar los libros, pero falta la implementación
+        }   
     }
 }
